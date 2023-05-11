b9976ac2a0f6795fafdce467b394db64b6cad638
Server Khalilov <red17electro@gmail.com>
2023-05-11 18:51:31 +0200

    upd: login / logout;

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -7,13 +7,13 @@ import bcrypt from "bcryptjs";
 import { db } from "./db.server";
 
 type LoginForm = {
-    username: string;
     password: string;
+    username: string;
 };
 
 export async function login({
-                                username,
                                 password,
+                                username,
                             }: LoginForm) {
     const user = await db.user.findUnique({
         where: { username },
@@ -21,6 +21,7 @@ export async function login({
     if (!user) {
         return null;
     }
+
     const isCorrectPassword = await bcrypt.compare(
         password,
         user.passwordHash
@@ -51,6 +52,60 @@ const storage = createCookieSessionStorage({
         httpOnly: true,
     },
 });
+
+function getUserSession(request: Request) {
+    return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function getUserId(request: Request) {
+    const session = await getUserSession(request);
+    const userId = session.get("userId");
+    if (!userId || typeof userId !== "string") {
+        return null;
+    }
+    return userId;
+}
+
+export async function requireUserId(
+    request: Request,
+    redirectTo: string = new URL(request.url).pathname
+) {
+    const session = await getUserSession(request);
+    const userId = session.get("userId");
+    if (!userId || typeof userId !== "string") {
+        const searchParams = new URLSearchParams([
+            ["redirectTo", redirectTo],
+        ]);
+        throw redirect(`/login?${searchParams}`);
+    }
+    return userId;
+}
+
+export async function getUser(request: Request) {
+    const userId = await getUserId(request);
+    if (typeof userId !== "string") {
+        return null;
+    }
+
+    try {
+        const user = await db.user.findUnique({
+            select: { id: true, username: true },
+            where: { id: userId },
+        });
+        return user;
+    } catch {
+        throw logout(request);
+    }
+}
+
+export async function logout(request: Request) {
+    const session = await getUserSession(request);
+    return redirect("/login", {
+        headers: {
+            "Set-Cookie": await storage.destroySession(session),
+        },
+    });
+}
 
 export async function createUserSession(
     userId: string,
